26e1f65edd0ce51a17561c5fd8060063cdf51f1b
Finn Kumkar <finn.kumkar@gmail.com>
2022-07-11 23:43:10 +0200

    Simplify installation

--- provisioning/install_wsl_debian.ps1
+++ provisioning/install_wsl_debian.ps1
@@ -12,22 +12,6 @@ if (!$windowsIdentity.IsInRole([Security.Principal.WindowsBuiltInRole] "Administ
 # This script installs the Windows Subsystem for
 # Linux (WSL) and the official Debian App.
 #
-# https://docs.microsoft.com/en-us/windows/wsl/wsl2-install
-# https://docs.microsoft.com/en-us/windows/wsl/install-manual
-# https://www.microsoft.com/en-us/p/debian/9msvkqc78pk6
-#
+# https://docs.microsoft.com/en-us/windows/wsl/install
 
-dism.exe /online `
-         /enable-feature `
-         /featurename:Microsoft-Windows-Subsystem-Linux `
-         /all `
-         /norestart
-
-curl.exe --location `
-         --progress-bar `
-         --output "${env:TEMP}\Debian.appx" `
-         https://aka.ms/wsl-debian-gnulinux
-
-Add-AppxPackage "${env:TEMP}\Debian.appx"
-
-Start-Process debian
+wsl.exe --install --distribution debian
